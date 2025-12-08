5c79c9f8d04c20dd3abe422e4b40edde357eca9e
Danilov Nikita <danilovn228@gmail.com>
2025-12-08 23:27:05 +0500

    Update fulltext_test.sql
    Clean up old tables and reintroduce the fulltext test script.

--- fulltext_test.sql
+++ fulltext_test.sql
@@ -1,3 +1,7 @@
+-- Clean up old tables
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS books_no_index;
+
 -- SECTION 1: TABLE WITH FULLTEXT INDEX
 
 -- 2. Create the 'books' table
@@ -54,6 +58,5 @@ SELECT * FROM books;
 SELECT '--- Test 2: Search without Index (LIKE) ---' AS test_type; 
 EXPLAIN SELECT * FROM books_no_index 
 WHERE title LIKE '%Book10%'; 
-
 
 SELECT * FROM books_no_index WHERE title LIKE '%Book10%' LIMIT 5;
